b04d98eed38e0909b7c049c223fc29d8bd4fe575
Kostiantyn Oleksiienko <kostiantyn_oleksiienko@epam.com>
2020-07-06 17:22:16 +0300

    New repo

---
+++ main.tf
@@ -0,0 +1,104 @@
+# Variables
+variable "myregion" {
+  default = "us-east-1"
+}
+
+variable "accountId" {
+  default = "176065828214"
+}
+
+provider "aws" {
+  version = ">= 2.58"
+  region = var.myregion
+}
+
+# API Gateway
+resource "aws_api_gateway_rest_api" "api" {
+  name = "myapi"
+}
+
+#resource "aws_api_gateway_resource" "resource" {
+#  path_part   = "resource"
+#  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+#  rest_api_id = aws_api_gateway_rest_api.api.id
+#}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_rest_api.api.root_resource_id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+#  resource_id             = aws_api_gateway_resource.resource.id
+  resource_id             = aws_api_gateway_rest_api.api.root_resource_id
+  http_method             = aws_api_gateway_method.method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
+##### deploy plus stage ##############
+resource "aws_api_gateway_stage" "prod" {
+  stage_name    = "prod"
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  deployment_id = aws_api_gateway_deployment.dev.id
+}
+
+resource "aws_api_gateway_deployment" "dev" {
+  depends_on  = [aws_api_gateway_integration.integration]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "dev"
+}
+
+# Lambda
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}/"
+}
+
+resource "aws_lambda_function" "lambda" {
+  filename      = "lambda.zip"
+  function_name = "mylambda"
+  role          = aws_iam_role.role.arn
+  handler       = "lambda_function.lambda_handler1"
+  runtime       = "python2.7"
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda.zip"))}"
+  source_code_hash = filebase64sha256("lambda.zip")
+}
+
+# IAM
+resource "aws_iam_role" "role" {
+  name = "myrole"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+
+}
+
+output "invoke_url" {
+  value = aws_api_gateway_stage.prod.invoke_url
+}
